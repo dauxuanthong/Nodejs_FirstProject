d3d6e20fe12bc4fa15b6fa50eb7fd001fee5330e
SexyMeoww <SexyMeoww@DESKTOP-K1JGHBT>
2021-03-21 17:49:27 +0700

    library5

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
-        const newsController = require('../app/controllers/NewsController');
+const newsController = require('../app/controllers/NewsController');
 
 //slug news
 router.use('/:slug', newsController.show);
 // newController.index
 router.use('/', newsController.index);
 
-module.exports = router;
+  module.exports = router;
